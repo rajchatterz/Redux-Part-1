610514c65021326343122bb7d2b39982d226d6e3
rajchatterz <iamraj.space@gmail.com>
2024-01-01 16:04:29 +0530

    succesfuly fetch the data from flastlist

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Product } from '../component/ProductDetails'
 const HomeScreen = () => {
+    const renderData = ({item}) => (
+        <View>
+            <Text>{item.name}</Text>
+            <Image source={{uri:item.image}} style={{width: 200, height: 300,}}/>
+        </View>
+    )
   return (
     <View>
-          {Product.map((item, index) => (
-              <View>
-                  <Text>{ item.name}</Text>
-              </View>
-      ))}
+          <FlatList
+              data={Product}
+              renderItem={renderData}
+              keyExtractor={(item,index)=>index.toString()}
+          />
     </View>
   )
 }
